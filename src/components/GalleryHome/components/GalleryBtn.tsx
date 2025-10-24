7c8c43bf68857dddcd27a668636fbb23b80d9ae3
nasa14119 <nico.amaya0205@gmail.com>
2025-10-24 08:12:40 -0600

    styles(changes) btn color

--- src/components/GalleryHome/components/GalleryBtn.tsx
+++ src/components/GalleryHome/components/GalleryBtn.tsx
@@ -6,18 +6,14 @@ type Props = {
 const className = "text-center py-2 w-fit mx-auto px-5 rounded-4xl mb-2";
 export function GalleryBtn({ date = null }: Props) {
   if (date === null)
-    return <span className={className + " bg-red-800"}>Ver Imagenes</span>;
+    return <span className={className + " bg-accent"}>Ver Imagenes</span>;
   const parse_str = parse(date, "DD/MM/YYYY");
   let day = format(parse_str, "dddd, DD", "es");
-  let month = format(parse_str, "MMMM");
+  let month = format(parse_str, "MMMM", "es");
   return (
-    <span
-      className={
-        className + " bg-red-800/40 text-white/50 italic flex flex-col"
-      }
-    >
+    <span className={className + " bg-accent/40 text-white/50  flex flex-col"}>
       <span className="">Proximo evento</span>
-      <span className="first-letter:uppercase">{`${day} de ${month}`}</span>
+      <span className="italic first-letter:uppercase">{`${day} de ${month}`}</span>
     </span>
   );
 }
